224a7673c806a4d76915cdec2fcb7c3e01824bfe
jcsgh <business.joshuac@gmail.com>
2022-02-11 10:52:17 -0600

    rough homepage

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,6 +6,7 @@ const NavBar = () => {
         <nav style={container}>
             <Link to="/home">Home</Link>
             <Link to="/about">About</Link>
+            <img src="//images.squarespace-cdn.com/content/v1/5c881f812727be4b51cfb626/01a188a5-7882-49bc-bccd-e4beabd23c61/+MCL+Logo+scaled+down-01.png?format=1500w" style={img}></img>
             <Link to="/tour">Tour</Link>
             <Link to="/contact">Contact</Link>
         </nav>
@@ -15,6 +16,12 @@ const NavBar = () => {
 const container = {
     display: 'flex',
     justifyContent: 'space-around',
-  }
+}
+
+const img = {
+    width: "264px",
+    maxWidth: "264px",
+    height: "auto"
+}
 
 export default NavBar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,52 @@
 const Home = () => {
     return(
         <div>
-            <h1>Home</h1>
+            <h1 style={title}>Featuring Contestants from</h1>
+            <h1 style={title}> MasterChef and MasterChef Junior</h1>
+
+            <button style={button}>Buy tickets</button>
+
+            <div style={imgContainer}>
+                <img src="https://images.squarespace-cdn.com/content/v1/5c881f812727be4b51cfb626/1633982959732-SOUSLPWK1VYC3EXWXKDQ/NEW+ART+FT.+gerron+-01.jpg" style={img}></img>
+            </div>
         </div>
     )
+}
+
+const imgContainer = {
+    position: 'relative',
+    paddingBottom: '74.46428680419922%',
+    overflow: 'hidden'
+}
+
+const img = {
+    left: '0%',
+    top: '-0.000858516%',
+    width: '100%',
+    height: '100.002%',
+    position: 'absolute'
+}
+
+const button = {
+    backgroundColor: '#5fc3b1',
+    borderColor: '#5fc3b1',
+    color: '#fff',
+    borderRadius: '300px',
+    padding: "21px 41px",
+    fontFamily: "Helvetica",
+    fontSize: "15px",
+    fontWeight: "400",
+    textTransform: "uppercase",
+    letterSpacing: "3px",
+    display: "inline-block",
+    width: "auto",
+    height: "auto"
+}
+
+const title = {
+    color: "#e55400",
+    fontFamily: "helvetica",
+    fontSize: "60px"
 }
 
 export default Home;
